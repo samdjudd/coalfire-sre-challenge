3ef30abef8101f3ff97e9abb5b97375e49d99b92
Sam Judd <43186441+samdjudd@users.noreply.github.com>
2024-01-29 09:48:07 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source   = "./modules/vpc"
+  region   = "us-west-1"
+  vpc_cidr = "10.1.0.0/16"
+}
+
+
+module "subnets" {
+  source               = "./modules/subnets"
+  vpc_id               = module.vpc.vpc_id
+  availability_zones   = ["us-west-1a", "us-west-1c"]
+}
+
+module "ec2" {
+  source            = "./modules/ec2"
+  public_subnet_ids = module.subnets.public_subnet_ids
+  wp_subnet_ids     = module.subnets.wp_subnet_ids
+}
+
+module "alb" {
+  source = "./modules/alb"
+}
